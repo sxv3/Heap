bbd7cd068a34cc357fdd93079347d6ae24833761
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-02-19 21:49:11 -0800

    comments added to Heap.h

--- Heap.h
+++ Heap.h
@@ -6,25 +6,30 @@
 #include <cstdio>
 #include <cstdlib>
 
+//header file for the Heap class (manages the heap)
+//by Tai Wong, Feb 19th
+
 using namespace std;
 
+//class definition
 class Heap {
 private:
+//creates heap with max size of 100
   int heap[101];
   int size = 0;
 
-  void heapUp(int index);
-  void heapDown(int index);
-  void utilPrint(int index, int depth);
+  void heapUp(int index); // restore heap after insert
+  void heapDown(int index); //restore heap after delete
+  void utilPrint(int index, int depth); //print out heap
 
 public:
-  Heap();
-  void insert(int value);
-  void removeMax();
-  void removeAll();
-  void printHeap();
+  Heap(); // constructor
+  void insert(int value); //inser value to heap
+  void removeMax(); //remove the root element
+  void removeAll(); //remove all elements
+  void printHeap(); // print heap
 
-  void fileRead(const char* filename);
+  void fileRead(const char* filename); //read in file
   
 }
 
